653579a901eb1ee861c62e54e67c51113feeca0d
Eddie <eherrerag.eh@gmail.com>
2019-02-02 13:13:51 -0700

    Changed from Swing to JavaFX, and configured gardle.

--- src/main/java/UserInterface/SimpleGui.java
+++ src/main/java/UserInterface/SimpleGui.java
@@ -2,32 +2,29 @@ package UserInterface;
 
 import priceWatcherModel.Item;
 
-import java.awt.Color;
-import javax.swing.*;
 
-public class SimpleGui extends JFrame implements UI {
-    
-    private static final long serialVersionUID = 1L;
-    private JPanel panel;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SimpleGui extends Application implements UI {
+
 
     @Override
     public void draw() {
-       panel = new JPanel();
-        this.add(panel);
-        this.setSize(400, 500);
+        launch();
     }
 
     @Override
     public void addItem(Item item) {
-        ItemCell itemCell = new ItemCell(item);
-        itemCell.setBackground(Color.BLACK);
-        itemCell.setBounds(0, 0, 20, 20);
-        this.add(itemCell);
-        panel.add(new JButton());
-        SwingUtilities.updateComponentTreeUI(this);
-        this.setVisible(true);
-
 
     }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Price Watcher");
+        primaryStage.show();
+    }
+   
 
 }
--- src/main/java/priceWatcherConsole/Main.java
+++ src/main/java/priceWatcherConsole/Main.java
@@ -32,6 +32,7 @@ public class Main{
         else{
             main(args);
         }
+        scan.close();
 
         
         
