17122e835f2696cc329fd318c9d4b970e5c15645
Aaron Mendez <aamendez3@miners.utep.edu>
2019-05-05 19:28:17 -0600

    added some changes to the ItemPanel file

--- src/main/java/UserInterface/GUI/ItemPanel.java
+++ src/main/java/UserInterface/GUI/ItemPanel.java
@@ -34,33 +34,31 @@ public class ItemPanel extends JPanel {
         this.itemManager = itemManager;
 
         try {
-            itemManager.addItems(new Item("Sony Tv", new URL(
-                    "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
+            itemManager.addItems(new Item("MSP430", new URL(
+                    "https://www.ebay.com/itm/Texas-Instruments-MSP-EXP430G2-MSP430-Value-Line-LaunchPad-Development-Tool/202616780182?_trkparms=aid%3D222007%26algo%3DSIM.MBE%26ao%3D2%26asc%3D20160323102634%26meid%3D6fcd86cbce8e461a95499abce6a5a109%26pid%3D100623%26rk%3D5%26rkt%3D6%26sd%3D171074970306%26itm%3D202616780182&_trksid=p2047675.c100623.m-1"),
                     300, 200),
                     new Item("Sony Tv", new URL(
-                    "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
-                    300, 200)
-                    ,
+                            "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
+                            300, 200),
                     new Item("Sony Tv", new URL(
-                    "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
-                    300, 200)
-                    ,
+                            "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
+                            300, 200),
                     new Item("Sony Tv", new URL(
-                    "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
-                    300, 200)
-                    ,
+                            "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
+                            300, 200),
                     new Item("Sony Tv", new URL(
-                    "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
-                    300, 200));
+                            "https://www.bestbuy.com/site/samsung-65-class-led-nu8000-series-2160p-smart-4k-uhd-tv-with-hdr/6199828.p?skuId=6199828"),
+                            300, 200));
             listModel = new DefaultListModel<>();
-            for (Item item : itemManager.getItems())listModel.addElement(item);
+            for (Item item : itemManager.getItems())
+                listModel.addElement(item);
             jList = new JList<>(listModel);
             jList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
             jList.setCellRenderer(new ItemRenderer());
-            
+
             scrollPanel = new JScrollPane(jList);
             add(scrollPanel, BorderLayout.CENTER);
-            
+
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
